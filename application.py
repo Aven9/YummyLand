a9f8a19f961c4ea1b4138ab64d8171152ff24d84
Aven9 <961273881@qq.com>
2019-06-22 14:40:33 +0800

    test client layout

--- application.py
+++ application.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 from flask_session import Session
+from flask_jsglue import JSGlue
 
 app = Flask(__name__)
+jsglue = JSGlue(app)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -11,3 +13,23 @@ Session(app)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+
+@app.route('/shop_category/<string:category>')
+def shop_category(category):
+    return render_template('shopCategory.html', category=category)
+
+
+@app.route('/shopping_cart')
+def shopping_cart():
+    return render_template('shoppingCart.html')
+
+
+@app.route('/setting')
+def setting():
+    return render_template('setting.html')
