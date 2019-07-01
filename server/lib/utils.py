2bcdb724b5d7ad0adeb8cb0e2b22c3f845ba85df
liuchaorun <chaorunliu@gmail.com>
2019-07-01 11:56:23 +0800

    修改接口

--- server/lib/utils.py
+++ server/lib/utils.py
@@ -1,5 +1,31 @@
 from flask import jsonify
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 
 
 def returns(code, data, msg):
     return jsonify({'code': code, 'data': data, 'msg': msg})
+
+
+def send_email(to_account, subject, content):
+    # 1. 实例化SMTP
+    smtp = smtplib.SMTP()
+
+    # 2. 链接邮件服务器
+    smtp.connect("smtp.163.com")
+
+    # 3. 配置发送邮箱的用户名和密码
+    smtp.login("yunnnzhan@163.com", "yunzhan123456")
+
+    # 4. 配置发送内容msg
+    msg = MIMEText(content, 'plain', 'utf-8')
+    msg['Subject'] = Header(subject,'utf-8')
+    msg['From'] = "yunnnzhan@163.com"
+    msg['To'] = to_account
+
+    # 5. 配置发送邮箱，接受邮箱，以及发送内容
+    smtp.sendmail("yunnnzhan@163.com", to_account, msg.as_string())
+
+    # 6. 关闭邮件服务
+    smtp.quit()
--- server/routes/user.py
+++ server/routes/user.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, session, request
 import random
+import smtplib
 
-from ..lib.utils import returns
+from ..lib.utils import returns, send_email
 
 user = Blueprint('user', __name__)
 
@@ -11,9 +12,24 @@ def verification_code():
     data = request.get_json()
     if data.__contains__('email'):
         session['code'] = random.randint(1000, 9999)
-
+        try:
+            send_email(data['email'], '验证码', session['code'])
+            return returns(0, {}, '成功')
+        except smtplib.SMTPException:
+            return returns(5, {}, '邮件发送错误！')
     else:
-        returns(4, {}, '账号已被注册！')
+        return returns(6, {}, '缺少参数！')
 
+
+@user.route('/register', method=['post'])
+def register():
+    data = request.get_json()
+    if data.__contains__('email') and data.__contains__('username') and data.__contains__('password') and data.__contains__('code'):
+        if data['code'] == session['code']:
+            return returns(0, {}, '成功！')
+        else:
+            return returns(7, {}, '验证码错误！')
+    else:
+        return returns(6, {}, '缺少参数！')
 
 
